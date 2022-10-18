ff3369df048543496f834ca3ce7a86328a599692
Christian Bonilla <Alton77@csu.fullerton.edu>
2022-10-18 12:42:41 -0400

    all done as well!

--- part-2/guessing_game.cc
+++ part-2/guessing_game.cc
@@ -1,40 +1,49 @@
-
-// TODO: Insert your header.
+// Christian Alton Bonilla
+// CPSC 120-01
+// 2022-10-12
+// Alton77@csu.fullerton.edu
+// @alton7759
+//
+// Lab 06-02
+// Partners: @jaylinmai
+//
+// guessing game
+//
 
 #include <cmath>
 #include <iostream>
 #include <string>
 
-// TODO: Declare a const int named kSecretNumber and initialize it to 42.
-
+int k_secret_number = 42;
 int main(int argc, char const *argv[]) {
   int guess = 0;
   int last_guess = 0;
   std::string answer;
+  bool is_game_over{false};
 
   std::cout << "Hi - I'm a computer and I've thought of a number between "
                "1 and 100.\n";
   std::cout << "Let's play a guessing game...\n";
+  while (!is_game_over) {
+    std::cout << "What's your guess?> ";
+    std::cin >> guess;
 
-  // TODO: Write an loop that will continue until the player decides to quit
-  // TODO: Inside the loop, prompt the player for their guess
-  //       for example     std::cout << "What's your guess?> ";
-  //       If the player enters a number less than 1 or greater than 100, prompt
-  //       the player to enter a number again. You can do this with another
-  //       loop.
-  // TODO: Store the input from the keyboard into the variable guess
-  // TODO: Check to see if guess has the same value as kSecretNumber
-  //       If so, then the player has won and they can play again
-  //       Prompt the player if they would like to continue playing. If the
-  //       answer is "y" then restart the game. Otherwise, any other letter will
-  //       cause the game to end and the program to exit.
-  // TODO: Else, the player didn't win so they need to know if they are
-  //       getting warmer or colder.
-  //       Check to see if kSecretNumber - guess is less than kSecretNumber -
-  //       last_guess If it is, print you're getting warmer. Otherwise, print
-  //       you're getting colder.
-  // TODO: At the end of every iteration of the loop, save the current guess
-  //       into the variable last_guess.
+    if (k_secret_number == guess) {
+      std::cout << "Hooray! You guessed the secret number!!\n";
+      std::cout << "Do you want to play again? (y or n)> ";
+      std::cin >> answer;
+      if (answer == "n") {
+        return 0;
+      }
+    }
 
+    if (std::abs(k_secret_number - guess) <
+        std::abs(k_secret_number - last_guess)) {
+      std::cout << "You're getting warmer!\n";
+    } else {
+      std::cout << "You're getting colder.\n";
+    }
+    last_guess = guess;
+  }
   return 0;
 }
